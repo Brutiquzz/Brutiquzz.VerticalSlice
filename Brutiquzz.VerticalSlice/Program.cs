eb655a8e54e6d07098f781af385dc5e31ce2147c
thomas poulsen <polle_kiks@hotmail.com>
2024-04-21 22:43:21 +0200

    something

--- Brutiquzz.VerticalSlice/Program.cs
+++ Brutiquzz.VerticalSlice/Program.cs
@@ -1,6 +1,11 @@
+using FluentValidation;
+
 var builder = WebApplication.CreateBuilder(args);
+var assembly = typeof(Program).Assembly;
 
 // Add services to the container.
+builder.Services.AddMediatR(cf => cf.RegisterServicesFromAssembly(assembly));
+builder.Services.AddValidatorsFromAssembly(assembly);
 
 var app = builder.Build();
 
